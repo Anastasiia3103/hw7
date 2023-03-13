59b7ff5e1b1267e4df8e82cc7e570ecab5613f93
Anastasiia3103 <123423011+Anastasiia3103@users.noreply.github.com>
2023-03-13 18:14:02 +0100

    Update Cat.java

--- src/hw7/Cat.java
+++ src/hw7/Cat.java
@@ -4,23 +4,28 @@ public class Cat extends Animals {
     private final int MAX_RUN_DISTANCE = 200;
     private final int MAX_SWIM_DISTANCE = 0;
     private static int catCount;
-    public Cat(String name) {
-        super(name);
+
+    public Cat (String name) {
+        super (name);
         catCount++;
     }
-@Override
-    public void run(int distance) {
-        if (distance > MAX_RUN_DISTANCE) {
-            System.out.println("This cat can't run that far.");
-        } else {
-            System.out.println("This cat ran " + distance + " m.");
+
+    @Override
+    public void run (int distance) {
+        if ( distance > MAX_RUN_DISTANCE ) {
+            System.out.println ("This cat can't run that far.");
+        }
+        else {
+            System.out.println ("This cat ran " + distance + " m.");
         }
     }
-@Override
-    public void swim(int distance) {
-        System.out.println("This cat can't swim.");
+
+    @Override
+    public void swim (int distance) {
+        System.out.println ("This cat can't swim.");
     }
-    public static int getCatCount() {
+
+    public static int getCatCount () {
         return catCount;
     }
 }
