91b83b243f86ffde00763f4257464c9d3d62b21b
Anastasiia3103 <123423011+Anastasiia3103@users.noreply.github.com>
2023-03-13 18:14:30 +0100

    Update Dog.java

--- src/hw7/Dog.java
+++ src/hw7/Dog.java
@@ -4,28 +4,33 @@ public class Dog extends Animals {
     private final int MAX_RUN_DISTANCE = 500;
     private final int MAX_SWIM_DISTANCE = 10;
     private static int dogCount;
-    public Dog(String name) {
-        super(name);
+
+    public Dog (String name) {
+        super (name);
         dogCount++;
     }
 
     @Override
-    public void run(int distance) {
-        if (distance > MAX_RUN_DISTANCE) {
-            System.out.println("This dog can't run that far.");
-        } else {
-            System.out.println("This dog ran " + distance + " m.");
+    public void run (int distance) {
+        if ( distance > MAX_RUN_DISTANCE ) {
+            System.out.println ("This dog can't run that far.");
+        }
+        else {
+            System.out.println ("This dog ran " + distance + " m.");
         }
     }
+
     @Override
-    public void swim(int distance) {
-        if (distance > MAX_SWIM_DISTANCE) {
-            System.out.println("This dog can't swim that far.");
-        } else {
-            System.out.println("This dog swam " + distance + " m.");
+    public void swim (int distance) {
+        if ( distance > MAX_SWIM_DISTANCE ) {
+            System.out.println ("This dog can't swim that far.");
+        }
+        else {
+            System.out.println ("This dog swam " + distance + " m.");
         }
     }
-    public static int getDogCount() {
+
+    public static int getDogCount ( ) {
         return dogCount;
     }
 }
